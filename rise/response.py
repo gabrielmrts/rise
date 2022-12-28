18b94f92f9ea60cee8ee2ad6fb6282758b2b54c1
Gabriel <gabrielmrtss@gmail.com>
2022-12-28 00:47:37 -0300

    Response object refactoring

--- rise/response.py
+++ rise/response.py
@@ -1,13 +1,28 @@
 import typing as t
+from rise import status
 
 class Response:
 
     def __init__(self) -> None:
         self.body = {}
-        self.status = '200 OK'
-        self.headers: t.List[t.Tuple] = [('Content-Type', 'text/plain')]
+        self.status = status.HTTP_200_OK
+        self.headers: t.List[t.Tuple] = []
 
     def get_content_length(self) -> str:
+        """
+        Get the body size
+        """
+        
         body_size = len(str(self.body))
 
         return str(body_size)
+    
+    def get_content_type(self) -> str:
+        """
+        Get the body response content type
+        """
+
+        if isinstance(self.body, dict):
+            return "application/json"
+        else:
+            return "text/plain"
